4e8aab2969bd3452ea7f16c0a8b48b76a88fc382
SpaceEnthusiast <hanznathanpo@gmail.com>
2022-04-06 11:06:40 -0400

    Add vowel methods

--- src/WorkingWithStrings.java
+++ src/WorkingWithStrings.java
@@ -6,16 +6,21 @@ public class WorkingWithStrings {
 		
 		Scanner input = new Scanner(System.in);
 		
+		System.out.println("Input:");
 		String userInput = input.nextLine();
 		
 		input.close();
 		
+		System.out.println("Output:");
+		System.out.println("======================");
 		System.out.println(userInput);
 		
 		System.out.println(userInput.toUpperCase());
 		System.out.println(userInput.toLowerCase());
 		System.out.println(countWords(userInput));
 		System.out.println(alternateCase(userInput));
+		System.out.println(countVowels(userInput));
+		System.out.println(vowelToUpper(userInput));
 	}
 
 	public static int countWords(String value) {
@@ -38,5 +43,33 @@ public class WorkingWithStrings {
 			}
 		}
 		return String.valueOf(alternatedString);
+	}
+	
+	public static boolean isVowel(char value) {
+		return (value == 'a' || value == 'e' || value == 'i' || value == 'o' || value == 'u');
+	}
+	
+	public static int countVowels(String value) {
+		
+		int vowelCount = 0;
+		for (int i = 0; i < value.length(); i++) {
+			if (isVowel(value.charAt(i)))
+				vowelCount++;
+		}
+		
+		return vowelCount;
+	}
+	
+	public static String vowelToUpper (String value) {
+		char[] valueArray = new char[value.length()];
+		
+		for (int i = 0; i < value.length(); i++) {
+			if (isVowel(value.charAt(i)))
+				valueArray[i] = Character.toUpperCase(value.charAt(i));
+			else
+				valueArray[i] = Character.toLowerCase(value.charAt(i));
+		}
+		
+		return String.valueOf(valueArray);
 	}
 }
