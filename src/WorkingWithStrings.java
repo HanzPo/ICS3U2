c6dd1183e1f3e4fa30637918af314932cb3d5209
SpaceEnthusiast <hanznathanpo@gmail.com>
2022-04-07 09:49:27 -0400

    Add word stack

--- src/WorkingWithStrings.java
+++ src/WorkingWithStrings.java
@@ -5,27 +5,33 @@ public class WorkingWithStrings {
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
+		String[] searchWords = new String[] { "be", "the" };
 		
-		System.out.println("Input:");
+		System.out.println("Please enter a word with an even number of letters, or a phrase:");
 		String userInput = input.nextLine();
 		
 		input.close();
 		
 		System.out.println("Output:");
 		System.out.println("======================");
-		System.out.println(userInput);
 		
-		System.out.println("       Upper: " + userInput.toUpperCase());
-		System.out.println("       Lower: " + userInput.toLowerCase());
-		System.out.println("     # Words: " + countWords(userInput));
-		System.out.println("    Alt Case: " + alternateCase(userInput));
-		System.out.println("    # Vowels: " + countVowels(userInput));
-		System.out.println("  Vowel Case: " + vowelToUpper(userInput));
-		System.out.println("     Reverse: " + reverseString(userInput));
-		
-		String[] searchWords = new String[] { "be", "the" };
-		
-		System.out.println(countWords(userInput, searchWords));
+		if (countWords(userInput) > 1) {
+			System.out.println("     You entered: " + userInput);
+			System.out.println("           Upper: " + userInput.toUpperCase());
+			System.out.println("           Lower: " + userInput.toLowerCase());
+			System.out.println("         # Words: " + countWords(userInput));
+			System.out.println("        Alt Case: " + alternateCase(userInput));
+			System.out.println("        # Vowels: " + countVowels(userInput));
+			System.out.println("      Vowel Case: " + vowelToUpper(userInput));
+			System.out.println("         Reverse: " + reverseString(userInput));
+			System.out.println("  Be's and The's: " + countWords(userInput, searchWords));
+		}
+		else if (userInput.length() % 2 == 0) {
+			wordStack(userInput);
+		}
+		else {
+			System.out.println("Please enter a valid input");
+		}
 	}
 
 	public static int countWords(String value) {
@@ -101,5 +107,15 @@ public class WorkingWithStrings {
 		}
 		
 		return wordCount;
+	}
+	
+	public static void wordStack(String word) {
+		for (int i = 1; i <= (word.length() / 2); i++) {
+			for (int j = 0; j < (-1 * i) + word.length() / 2; j++)
+			{
+				System.out.print(" ");
+			}
+			System.out.printf("%s%n", word.substring(((word.length() / 2) - i), ((word.length() / 2) + i)));
+		}
 	}
 }
